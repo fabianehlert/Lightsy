1c2f1fa973a16d0830b40759364218d2fcdb2a17
Fabian Ehlert <fabian@fabianehlert.com>
2016-10-22 20:22:17 +0200

    Adds Xcode Project

---
+++ LightUUp/LightUUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightUUp
+//
+//  Created by Fabian Ehlert on 22.10.16.
+//  Copyright © 2016 Fabian Ehlert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
