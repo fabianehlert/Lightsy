e1704ec84cdeabd38920ef9a96406e1d5ffc784b
Fabian Ehlert <fabian@fabianehlert.com>
2016-10-24 19:36:33 +0200

    Adds LightView via Code

--- Lightsy/Lightsy/Controller/ViewController.swift
+++ Lightsy/Lightsy/Controller/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    fileprivate var lightView: LightView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.lightView = LightView(frame: self.view.frame)
+        if let l = self.lightView { self.view.addSubview(l) }
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.lightView?.frame = self.view.frame
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
