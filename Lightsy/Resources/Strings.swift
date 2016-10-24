4b82100099d14e9936e30aae343a4b83981bf9ff
Fabian Ehlert <fabian@fabianehlert.com>
2016-10-24 13:36:23 +0200

    Adds Strings resource file

---
+++ Lightsy/Resources/Strings.swift
@@ -0,0 +1,14 @@
+//
+//  Strings.swift
+//  Lightsy
+//
+//  Created by Fabian Ehlert on 24.10.16.
+//  Copyright © 2016 Fabian Ehlert. All rights reserved.
+//
+
+import Foundation
+
+enum Defaults: String {
+    case
+    firstAppLaunch = "letLightsyFirstAppLaunch"
+}
